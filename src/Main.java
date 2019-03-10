6bbbb7f5e129565fb95ffe281fd74d608957b235
Mateusz Wietrak <wietrak11@gmail.com>
2019-03-11 00:26:16 +0100

    Skonczony program

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,42 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj kąt w stopniach");
         double x = scanner.nextDouble();
-        System.out.println("Wynik Math.sin() gdzie podana liczba została zamieniona na radiany = " + Math.sin(Math.toRadians(x)));
-        System.out.println("Wynik Math.sin() bez zamiany na radiany = " + Math.sin(x));
+        System.out.println("Podaj liczbe elementow szeregu");
+        int dlugoscSzeregu = scanner.nextInt();
 
+        System.out.println("Wynik Math.sin() = " + Math.sin(Math.toRadians(x)));
+        System.out.println("Wynik własnej implementacji = " + obliczSin(x,dlugoscSzeregu));
+
+    }
+
+    public static double obliczSin(double x, int dlugoscSzeregu){
+        x = Math.toRadians(x);
+        double wynik = 0;
+
+        for(int i=0 ; i<dlugoscSzeregu ; i++){
+
+            if(i%4 == 0){
+                wynik += 0;
+            }
+            else if(i%4 == 1){
+                wynik += (Math.pow(x,i))/silnia(i);
+            }
+            else if(i%4 == 2){
+                wynik -= 0;
+            }
+            else if(i%4 == 3){
+                wynik -= (Math.pow(x,i))/silnia(i);
+            }
+
+        }
+        return wynik;
+    }
+
+    public static int silnia(int x)
+    {
+        if (x == 0)
+            return 1;
+        else
+            return x * silnia(x - 1);
     }
 }
