a051b1bfde3cbb0dc44722b233004631aea25bc3
Mateusz Wietrak <wietrak11@gmail.com>
2019-03-10 23:30:56 +0100

    obliczanie z uzyciem mathsin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj kąt w stopniach");
+        double x = scanner.nextDouble();
+        System.out.println("Wynik Math.sin() gdzie podana liczba została zamieniona na radiany = " + Math.sin(Math.toRadians(x)));
+        System.out.println("Wynik Math.sin() bez zamiany na radiany = " + Math.sin(x));
+
     }
 }
